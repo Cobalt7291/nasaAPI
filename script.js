71c6565f403821b0696e981bf7716768799c26f4
Cobalt <Cobalt7291@gmail.com>
2020-12-12 00:17:34 -0500

    jacob update

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// function DOMLoaded () {
+
+$(document).ready(function() {
     POTD();
-// }
+     //Jacobs API key = YXG9udc8M5QLeqooH25qwm8pkJ7FE4wRikvXk2hL
+
 
 function POTD () {
 
-    // const POTD_DIV = document.getElementById("pic-of-day");
+    
     const $POTD_DIV = $('#pic-of-day');
+   
 
-    queryURL = "https://api.nasa.gov/planetary/apod?date=2020-11-19&api_key=YXG9udc8M5QLeqooH25qwm8pkJ7FE4wRikvXk2hL"
+    queryURL = "https://api.nasa.gov/planetary/apod?&api_key=YXG9udc8M5QLeqooH25qwm8pkJ7FE4wRikvXk2hL"
 
     fetch(queryURL)
         .then(response => response.json())
@@ -16,19 +19,16 @@ function POTD () {
             console.log(potd);
 
             var potd = data.url;
-            // var imgClass = document.createElement('img');
             var imgClass = $('<img>');
-            // imgClass.setAttributes({
+            
              imgClass.attr({
                 'src': potd,
                 'alt': 'NASA Photo of the Day'
             });
 
-            // POTD_DIV.appendChild(imgClass);
             $POTD_DIV.append(imgClass);
             
         })
-        // .then(data => console.log(data.url));
-    //Jacobs API key = YXG9udc8M5QLeqooH25qwm8pkJ7FE4wRikvXk2hL
+         
 }
-
+});
