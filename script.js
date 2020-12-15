1b39180edaf01a46a4a75f4babdad590c4fac756
elenabaurkot <elenabaurkot@gmail.com>
2020-12-14 23:31:30 -0500

    current

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
+$(document).ready(function () {
+  POTD();
+  //Jacobs API key = YXG9udc8M5QLeqooH25qwm8pkJ7FE4wRikvXk2hL
 
-$(document).ready(function() {
-    POTD();
-     //Jacobs API key = YXG9udc8M5QLeqooH25qwm8pkJ7FE4wRikvXk2hL
+  function POTD() {
+    // const POTD_DIV = document.getElementById("pic-of-day");
+    const $POTD_DIV = $('#pic-of-day');
 
+    queryURL =
+      'https://api.nasa.gov/planetary/apod?&api_key=YXG9udc8M5QLeqooH25qwm8pkJ7FE4wRikvXk2hL';
 
-function POTD() {
-  // const POTD_DIV = document.getElementById("pic-of-day");
-  const $POTD_DIV = $('#pic-of-day');
+    fetch(queryURL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
 
-  queryURL =
-    'https://api.nasa.gov/planetary/apod?&api_key=YXG9udc8M5QLeqooH25qwm8pkJ7FE4wRikvXk2hL';
+        var potd = data.url;
+;
+        var imgClass = $('<img>');
 
-  fetch(queryURL)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      console.log(potd);
+        imgClass.attr({
+          src: potd,
+          alt: 'NASA Photo of the Day',
+        });
 
-      var potd = data.url;
-      // var imgClass = document.createElement('img');
-      var imgClass = $('<img>');
-      // imgClass.setAttributes({
-      imgClass.attr({
-        src: potd,
-        alt: 'NASA Photo of the Day',
+        $POTD_DIV.append(imgClass);
       });
-
-            
-
-            $POTD_DIV.append(imgClass);
-            
-        })
-         
-}
+  }
 });
